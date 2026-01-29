182bf4d8ad311a477319c5c1bb79a02941bc58f8
fima0519 <fimafariaewu>
2026-01-29 15:31:17 +0600

    feat: Why You Should Choose Agenc

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = ({ className = '', children }) => {
+    return (
+        <>
+            <div className={`max-w-[1320px] m-auto  ${className}`}>
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default Container
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Benefits from '../layouts/Benefits'
+
+const Home = () => {
+  return (
+    <Benefits/>
+  )
+}
+
+export default Home
